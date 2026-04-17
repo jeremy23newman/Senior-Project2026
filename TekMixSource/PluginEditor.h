78352e3ff2f8b5244ec2b0c7794a5a5fe87d86d3
jeremy23newman <jeremy23newman@gmail.com>
2026-04-16 20:17:39 -0400

    Add files via upload

--- TekMixSource/PluginEditor.h
+++ TekMixSource/PluginEditor.h
@@ -1,33 +1,52 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
 //==============================================================================
-/**
-*/
 class TekMixAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
-public:
+    public:
     TekMixAudioProcessorEditor (TekMixAudioProcessor&);
     ~TekMixAudioProcessorEditor() override;
-
+    
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
-
-private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
+    
+    // KNOBS
+    juce::Slider driveKnob, bitDepthKnob, delayKnob;
+    juce::Slider inputGainKnob, outputGainKnob, masterMixKnob, feedbackKnob;
+    
+    //LABELS
+    juce::Label driveLabel, bitDepthLabel, delayLabel;
+    juce::Label inputLabel, outputLabel, feedbackLabel, masterMixLabel, delaySectionLabel;
+    
+    //FILTER(SLIDER)
+    juce::Slider cutoffSlider;
+    juce::ToggleButton highpassButton;
+    
+    //BYPASS(BUTTONS)
+    juce::ToggleButton delayBypassButton;
+    juce::ToggleButton bitcrushBypassButton;
+    juce::ToggleButton overdriveBypassButton;
+    
+    //ATTACHMENT
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> driveAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> bitDepthAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> delayAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> feedbackAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> cutoffAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> inputAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> outputAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> mixAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> hpAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> delayBypassAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> bitcrushBypassAttach;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> overdriveBypassAttach;
+    
+    private:
     TekMixAudioProcessor& audioProcessor;
-
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TekMixAudioProcessorEditor)
 };
--- TekMixSource/PluginProcessor.cpp
+++ TekMixSource/PluginProcessor.cpp
@@ -1,10 +1,10 @@
 /*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
+ ==============================================================================
+ 
+ This file contains the basic framework code for a JUCE plugin processor.
+ 
+ ==============================================================================
+ */
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
@@ -14,34 +14,57 @@ juce::AudioProcessorValueTreeState::ParameterLayout
 TekMixAudioProcessor::createParameterLayout()
 {
     std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
-
-    // Delay
-    params.push_back(std::make_unique<juce::AudioParameterFloat>(
-        "delayTime", "Delay Time",
-        juce::NormalisableRange<float>(0.01f, 2.0f, 0.01f), 0.5f));
-
-    params.push_back(std::make_unique<juce::AudioParameterFloat>(
-        "wetDry", "Wet/Dry",
-        juce::NormalisableRange<float>(0.0f, 1.0f, 0.01f), 0.5f));
-
-    // Filter
-    params.push_back(std::make_unique<juce::AudioParameterFloat>(
-        "cutoff", "Cutoff",
-        juce::NormalisableRange<float>(20.0f, 20000.0f, 1.0f), 1000.0f));
-
-    params.push_back(std::make_unique<juce::AudioParameterBool>(
-        "highpass", "High Pass", false));
     
-    // Overdrive
+    //DELAY
     params.push_back(std::make_unique<juce::AudioParameterFloat>(
-        "drive", "Drive",
-        juce::NormalisableRange<float>(1.0f, 20.0f, 0.1f), 2.0f));
-
-    // Bitcrusher
+                                                                 "delayTime", "Delay Time",
+                                                                 juce::NormalisableRange<float>(0.01f, 2.0f, 0.01f), 0.5f));
+    
+    //FEEDBACK
     params.push_back(std::make_unique<juce::AudioParameterFloat>(
-        "bitDepth", "Bit Depth",
-        juce::NormalisableRange<float>(1.0f, 16.0f, 1.0f), 8.0f));
-
+                                                                 "feedback", "Feedback",
+                                                                 juce::NormalisableRange<float>(0.0f, 0.95f, 0.01f), 0.3f));
+    
+    //FILTER
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+                                                                 "cutoff", "Cutoff",
+                                                                 juce::NormalisableRange<float>(20.0f, 20000.0f, 1.0f), 1000.0f));
+    
+    params.push_back(std::make_unique<juce::AudioParameterBool>(
+                                                                "highpass", "High Pass", false));
+    
+    //OVERDRIVE
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+                                                                 "drive", "Drive",
+                                                                 juce::NormalisableRange<float>(1.0f, 20.0f, 0.1f), 2.0f));
+    
+    //BITCRUSHER
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+                                                                 "bitDepth", "Bit Depth",
+                                                                 juce::NormalisableRange<float>(1.0f, 16.0f, 1.0f), 8.0f));
+    
+    //GLOBAL
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+                                                                 "inputGain", "Input Gain",
+                                                                 juce::NormalisableRange<float>(0.0f, 2.0f, 0.01f), 1.0f));
+    
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+                                                                 "outputGain", "Output Gain",
+                                                                 juce::NormalisableRange<float>(0.0f, 2.0f, 0.01f), 1.0f));
+    
+    params.push_back(std::make_unique<juce::AudioParameterFloat>(
+                                                                 "masterMix", "Master Mix",
+                                                                 juce::NormalisableRange<float>(0.0f, 1.0f, 0.01f), 1.0f));
+    params.push_back(std::make_unique<juce::AudioParameterBool>(
+                                                                "bypassDelay", "Bypass Delay", false));
+    
+    params.push_back(std::make_unique<juce::AudioParameterBool>(
+                                                                "bypassBitcrush", "Bypass Bitcrush", false));
+    
+    params.push_back(std::make_unique<juce::AudioParameterBool>(
+                                                                "bypassOverdrive", "Bypass Overdrive", false));
+    
+    
     return { params.begin(), params.end() };
 }
 
@@ -49,16 +72,16 @@ TekMixAudioProcessor::createParameterLayout()
 // CONSTRUCTOR
 TekMixAudioProcessor::TekMixAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       ),
+: AudioProcessor (BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+                  .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
 #endif
-       apvts(*this, nullptr, "Parameters", createParameterLayout())
+                  .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+                  ),
+#endif
+apvts(*this, nullptr, "Parameters", createParameterLayout())
 {
 }
 
@@ -74,29 +97,29 @@ const juce::String TekMixAudioProcessor::getName() const
 
 bool TekMixAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
+#if JucePlugin_WantsMidiInput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool TekMixAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
+#if JucePlugin_ProducesMidiOutput
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 bool TekMixAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     return true;
-   #else
+#else
     return false;
-   #endif
+#endif
 }
 
 double TekMixAudioProcessor::getTailLengthSeconds() const
@@ -106,8 +129,8 @@ double TekMixAudioProcessor::getTailLengthSeconds() const
 
 int TekMixAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1;
+    
 }
 
 int TekMixAudioProcessor::getCurrentProgram()
@@ -132,16 +155,16 @@ void TekMixAudioProcessor::changeProgramName (int index, const juce::String& new
 void TekMixAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
     samplingRate = sampleRate;
-
-        // Delay buffer (2 seconds)
-        auto delayBufferSize = (int)(sampleRate * 2.0);
-        delayBuffer.setSize(getTotalNumOutputChannels(), delayBufferSize);
-        delayBuffer.clear();
-
-        // Filter state
-        dnBuffer.resize(getTotalNumOutputChannels(), 0.0f);
-
-        writePosition = 0;
+    
+    //DELAY BUFFER INTIALIZED
+    auto delayBufferSize = (int)(sampleRate * 2.0);
+    delayBuffer.setSize(getTotalNumOutputChannels(), delayBufferSize);
+    delayBuffer.clear();
+    
+    //FILTER STARTING POSITION
+    dnBuffer.resize(getTotalNumOutputChannels(), 0.0f);
+    
+    writePosition = 0;
     
 }
 
@@ -154,133 +177,160 @@ void TekMixAudioProcessor::releaseResources()
 #ifndef JucePlugin_PreferredChannelConfigurations
 bool TekMixAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
+#if JucePlugin_IsMidiEffect
     juce::ignoreUnused (layouts);
     return true;
-  #else
+#else
     // This is the place where you check if the layout is supported.
     // In this template code we only support mono or stereo.
     // Some plugin hosts, such as certain GarageBand versions, will only
     // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
-
+    
     // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
+#if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
-
+#endif
+    
     return true;
-  #endif
+#endif
 }
 #endif
 
 void TekMixAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
-
-        auto totalNumInputChannels  = getTotalNumInputChannels();
-        auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-        
-        for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-            buffer.clear (i, 0, buffer.getNumSamples());
-        
-        
+    
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+    
+    //STORE DRY SIGNAL FOR MASTER MIX
+    juce::AudioBuffer<float> dryBuffer;
+    dryBuffer.makeCopyOf(buffer);
+    
+    //INPUT GAIN
+    auto inputGain = apvts.getRawParameterValue("inputGain")->load();
+    buffer.applyGain(inputGain);
+    
     processOverdrive(buffer);
     processFilter(buffer);
     processBitcrush(buffer);
     processDelay(buffer);
     
+    //MASTER MIX
+    auto mix = apvts.getRawParameterValue("masterMix")->load();
+    
+    for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
+    {
+        for (int i = 0; i < buffer.getNumSamples(); ++i)
+        {
+            auto wet = buffer.getSample(ch, i);
+            auto dry = dryBuffer.getSample(ch, i);
+            
+            buffer.setSample(ch, i, dry * (1.0f - mix) + wet * mix);
+        }
+    }
+    
+    //OUTPUT GAIN
+    auto outputGain = apvts.getRawParameterValue("outputGain")->load();
+    buffer.applyGain(outputGain);
+    
     
 }
+//OVERDRIVE
 void TekMixAudioProcessor::processOverdrive(juce::AudioBuffer<float>& buffer)
 {
+    if (apvts.getRawParameterValue("bypassOverdrive")->load() > 0.5f)
+        return;
+    
     auto drive = apvts.getRawParameterValue("drive")->load();
-
+    
     for (int channel = 0; channel < buffer.getNumChannels(); ++channel)
     {
         auto* samples = buffer.getWritePointer(channel);
-
+        
         for (int i = 0; i < buffer.getNumSamples(); ++i)
         {
             auto x = samples[i] * drive;
-
+            
             
             samples[i] = std::tanh(x);
         }
     }
 }
-
+//BITCRUSH
 void TekMixAudioProcessor::processBitcrush(juce::AudioBuffer<float>& buffer)
 {
+    if (apvts.getRawParameterValue("bypassBitcrush")->load() > 0.5f)
+        return;
+    
     auto bitDepth = apvts.getRawParameterValue("bitDepth")->load();
-
+    
     auto levels = std::pow(2.0f, bitDepth);
-
+    
     for (int channel = 0; channel < buffer.getNumChannels(); ++channel)
     {
         auto* samples = buffer.getWritePointer(channel);
-
+        
         for (int i = 0; i < buffer.getNumSamples(); ++i)
         {
             auto x = samples[i];
-
-          
+            
+            
             samples[i] = std::round(x * levels) / levels;
         }
     }
 }
-
+//FILTER
 void TekMixAudioProcessor::processFilter(juce::AudioBuffer<float>& buffer)
 {
     constexpr auto PI = 3.14159265359f;
-
+    
     auto cutoff = apvts.getRawParameterValue("cutoff")->load();
     auto hp = apvts.getRawParameterValue("highpass")->load();
-
+    
     const auto sign = hp ? -1.f : 1.f;
-
+    
     const auto tanVal = std::tan(PI * cutoff / samplingRate);
     const auto a1 = (tanVal - 1.f) / (tanVal + 1.f);
-
+    
     for (int channel = 0; channel < buffer.getNumChannels(); ++channel)
     {
         auto* samples = buffer.getWritePointer(channel);
-
+        
         for (int i = 0; i < buffer.getNumSamples(); ++i)
         {
             auto input = samples[i];
-
+            
             auto allpass = a1 * input + dnBuffer[channel];
             dnBuffer[channel] = input - a1 * allpass;
-
+            
             samples[i] = 0.5f * (input + sign * allpass);
         }
     }
 }
-
+//DELAY
 void TekMixAudioProcessor::processDelay(juce::AudioBuffer<float>& buffer)
 {
-    auto totalNumInputChannels = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-    auto wetDry = apvts.getRawParameterValue("wetDry")->load();
-    auto dryGain = 1.0f - wetDry;
-
+    if (apvts.getRawParameterValue("bypassDelay")->load() > 0.5f)
+        return;
     
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-        buffer.applyGain(channel, 0, buffer.getNumSamples(), dryGain);
-
+    auto totalNumInputChannels = getTotalNumInputChannels();
+    
+    
+    
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         fillBuffer(buffer, channel);
         readFromBuffer(buffer, delayBuffer, channel);
-        fillBuffer(buffer, channel);
     }
-
+    
     updateBufferPositions(buffer, delayBuffer);
 }
 
@@ -289,7 +339,7 @@ void TekMixAudioProcessor::fillBuffer(juce::AudioBuffer<float>& buffer, int chan
 {
     auto bufferSize = buffer.getNumSamples();
     auto delayBufferSize = delayBuffer.getNumSamples();
-
+    
     if (delayBufferSize > bufferSize + writePosition)
     {
         delayBuffer.copyFrom(channel, writePosition,
@@ -298,12 +348,12 @@ void TekMixAudioProcessor::fillBuffer(juce::AudioBuffer<float>& buffer, int chan
     else
     {
         auto numSamplesToEnd = delayBufferSize - writePosition;
-
+        
         delayBuffer.copyFrom(channel, writePosition,
                              buffer.getWritePointer(channel), numSamplesToEnd);
-
+        
         auto numSamplesAtStart = bufferSize - numSamplesToEnd;
-
+        
         delayBuffer.copyFrom(channel, 0,
                              buffer.getWritePointer(channel, numSamplesToEnd),
                              numSamplesAtStart);
@@ -311,52 +361,47 @@ void TekMixAudioProcessor::fillBuffer(juce::AudioBuffer<float>& buffer, int chan
 }
 
 void TekMixAudioProcessor::readFromBuffer(
-    juce::AudioBuffer<float>& buffer,
-    juce::AudioBuffer<float>& delayBuffer,
-    int channel)
+                                          juce::AudioBuffer<float>& buffer,
+                                          juce::AudioBuffer<float>& delayBuffer,
+                                          int channel)
 {
     auto bufferSize = buffer.getNumSamples();
     auto delayBufferSize = delayBuffer.getNumSamples();
-
+    
     auto delayTime = apvts.getRawParameterValue("delayTime")->load();
     auto delaySamples = (int)(getSampleRate() * delayTime);
-
-    auto wetDry = apvts.getRawParameterValue("wetDry")->load();
-    auto wetGain = wetDry;
-
+    
+    auto feedback = apvts.getRawParameterValue("feedback")->load();
+    
     auto readPosition = writePosition - delaySamples;
     if (readPosition < 0)
         readPosition += delayBufferSize;
-
-    if (readPosition + bufferSize < delayBufferSize)
+    
+    
+    for (int i = 0; i < bufferSize; ++i)
     {
-        buffer.addFromWithRamp(channel, 0,
-            delayBuffer.getReadPointer(channel, readPosition),
-            bufferSize, wetGain, wetGain);
-    }
-    else
-    {
-        auto numSamplesToEnd = delayBufferSize - readPosition;
-
-        buffer.addFromWithRamp(channel, 0,
-            delayBuffer.getReadPointer(channel, readPosition),
-            numSamplesToEnd, wetGain, wetGain);
-
-        auto numSamplesAtStart = bufferSize - numSamplesToEnd;
-
-        buffer.addFromWithRamp(channel, numSamplesToEnd,
-            delayBuffer.getReadPointer(channel, 0),
-            numSamplesAtStart, wetGain, wetGain);
+        int delayIndex = (readPosition + i) % delayBufferSize;
+        int writeIndex = (writePosition + i) % delayBufferSize;
+        
+        auto delayedSample = delayBuffer.getSample(channel, delayIndex);
+        auto inputSample   = buffer.getSample(channel, i);
+        
+        
+        buffer.setSample(channel, i, delayedSample);
+        
+        
+        delayBuffer.setSample(channel, writeIndex,
+                              inputSample + delayedSample * feedback);
     }
 }
 
 void TekMixAudioProcessor::updateBufferPositions(
-    juce::AudioBuffer<float>& buffer,
-    juce::AudioBuffer<float>& delayBuffer)
+                                                 juce::AudioBuffer<float>& buffer,
+                                                 juce::AudioBuffer<float>& delayBuffer)
 {
     auto bufferSize = buffer.getNumSamples();
     auto delayBufferSize = delayBuffer.getNumSamples();
-
+    
     writePosition += bufferSize;
     writePosition %= delayBufferSize;
 }
